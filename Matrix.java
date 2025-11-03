eec6849e6d715c79113bbd5019501459f3d6d7d7
KunalKajrekar15 <kunalkajrekarbsc24@rvu.edu.in>
2025-11-03 11:58:27 +0530

    Update Matrix.java

--- Matrix.java
+++ Matrix.java
@@ -2,3 +2,8 @@ import java.util.Arrays;
 
 public class Matrix {
     private final double[][] a;
+ public final int rows, cols;
+    public Matrix(double[][] data) {
+        rows = data.length; cols = data[0].length; a = new double[rows][cols];
+        for (int i=0;i<rows;i++) a[i] = Arrays.copyOf(data[i], cols);
+    }
