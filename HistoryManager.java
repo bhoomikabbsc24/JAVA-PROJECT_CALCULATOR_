0ccb02042b63ae04ddc70c681e141275c8babe16
KunalKajrekar15 <kunalkajrekarbsc24@rvu.edu.in>
2025-11-04 18:52:39 +0530

    Update HistoryManager.java

--- HistoryManager.java
+++ HistoryManager.java
@@ -1,1 +1,10 @@
-// to add code later
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+
+public class HistoryManager {
+    private final Path historyFile;
+
+    public HistoryManager() {
+        this("history.txt");
+    }// to add code later
