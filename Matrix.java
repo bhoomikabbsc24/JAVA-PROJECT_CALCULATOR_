ed912865ccffc5f378b9a851424686792b9eae8f
KunalKajrekar15 <kunalkajrekarbsc24@rvu.edu.in>
2025-11-04 12:30:37 +0530

    Add determinant calculation to Matrix class

--- Matrix.java
+++ Matrix.java
@@ -13,3 +13,12 @@ public class Matrix {
         for (int i=0;i<rows;i++) for (int j=0;j<cols;j++) out[i][j] = a[i][j] + b.a[i][j];
         return new Matrix(out);
     }
+  public double determinant() { if (rows!=cols) throw new RuntimeException("Not square"); return det(a); }
+    private double det(double[][] m) {
+        int n = m.length;
+        if (n==1) return m[0][0];
+        if (n==2) return m[0][0]*m[1][1] - m[0][1]*m[1][0];
+        double sum=0;
+        for (int j=0;j<n;j++) sum += Math.pow(-1,j) * m[0][j] * det(minor(m,0,j));
+        return sum;
+    }
