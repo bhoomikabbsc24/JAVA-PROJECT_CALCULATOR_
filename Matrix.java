e16c9b668461d384ea0ff844abd4f5c41790de37
navyatulibsc24-oss <navyatulibsc24@rvu.edu.in>
2025-11-04 12:22:33 +0530

    Update Matrix.java

--- Matrix.java
+++ Matrix.java
@@ -7,3 +7,9 @@ public class Matrix {
         rows = data.length; cols = data[0].length; a = new double[rows][cols];
         for (int i=0;i<rows;i++) a[i] = Arrays.copyOf(data[i], cols);
     }
+ public Matrix add(Matrix b) {
+        if (rows!=b.rows || cols!=b.cols) throw new RuntimeException("Dimension mismatch");
+        double[][] out = new double[rows][cols];
+        for (int i=0;i<rows;i++) for (int j=0;j<cols;j++) out[i][j] = a[i][j] + b.a[i][j];
+        return new Matrix(out);
+    }
