a04a86a2007b74bb2e82b723525c2328e03e216d
bhumika5590 <144927555+bhumika5590@users.noreply.github.com>
2025-11-05 13:11:45 +0530

    Update ComplexNumber.java

--- ComplexNumber.java
+++ ComplexNumber.java
@@ -1,1 +1,5 @@
-// to add code later 
+public class ComplexNumber {
+    public final double re, im;
+    public ComplexNumber(double r, double i) { re = r; im = i; }
+    public ComplexNumber add(ComplexNumber o) { return new ComplexNumber(re+o.re, im+o.im); }
+    public ComplexNumber sub(ComplexNumber o) { return new ComplexNumber(re-o.re, im-o.im); }
